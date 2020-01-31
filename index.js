a2631ae0c82b3d10a1b9b5bb58f9d9a234e9032e
mmelihdogan <doganmmelih@gmail.com>
2020-01-31 19:02:53 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+
+function submitData(name, email){
+    let user = {
+    name: name,
+    email: email
+  };
+  
+  let configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(user)
+  };
+  
+  return fetch("http://localhost:3000/users", configurationObject)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    document.body.append(object.id);
+  })
+  .catch(function(error) {
+    document.body.append(error.message)
+  });
+
+}
