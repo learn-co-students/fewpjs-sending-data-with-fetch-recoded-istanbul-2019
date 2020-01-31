b4ff2202d15b916df147d63b0db0a8e177f7f4b0
Oktay Ibis <oktayibis@gmail.com>
2020-01-31 21:34:07 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,66 @@
 // Add your code here
+/**
+ * 
+let formData = {
+  dogName: "Byron",
+  dogBreed: "Poodle"
+};
+ 
+let configObj = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify(formData)
+};
+ 
+fetch("http://localhost:3000/dogs", configObj)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    console.log(object);
+  })
+  .catch(function(error) {
+    alert("Bad things! Ragnarők!");
+    console.log(error.message);
+  });
+ */
+
+ function submitData(name, mail){
+     let formData = {
+        name : name,
+        email: mail
+     }
+
+     let configObj = {
+         method: 'POST',
+         headers: {
+             'Content-Type' : 'application/json',
+             'Accept' : 'application/json'
+         },
+         body: JSON.stringify(formData)
+     }
+
+    return fetch('http://localhost:3000/users', configObj)
+    .then(function(reps){
+        return reps.json();
+    })
+    .then(function(object){
+        document.body.innerHTML = object.id;
+    })
+    .catch(function(error){
+        document.body.innerHTML = error.message;
+    });
+    
+    //json-server --watch db.json
+ }
+
+//  document.querySelector('#submit').addEventListener('click', function(){
+//      let name = document.querySelector('#name').value;
+//      let email = document.querySelector('#email').value;
+
+//      submitData(name,email);
+//  })
+
