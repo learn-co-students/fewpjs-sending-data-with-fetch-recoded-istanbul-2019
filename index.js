489995a0ae067095c98743d9ad6bc62ea290f505
Abdulrahman Y. idlbi <adlogi@gmail.com>
2020-02-01 12:52:24 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Add your code here
+function submitData(name, email) {
+    let formData = {
+        'name': name,
+        'email': email
+    };
+
+    let configObj = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(formData)
+    };
+    
+    return fetch('http://localhost:3000/users', configObj)
+        .then(function(respose) {
+            return respose.json();
+        })
+        .then(function(object) {
+            console.log(object);
+            const text = document.createElement('p');
+            text.textContent = object.id;
+            document.body.append(text);
+        })
+        .catch(function(error) {
+            console.log(error.message);
+            alert('Bad command!');
+            const text = document.createElement('p');
+            text.textContent = error.message;
+            document.body.append(text);
+        });
+}
+
+done();
