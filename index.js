4e430fe7dc2539b5f2b3fa89159ce0682d64df89
Recep Öztürk <turkishteacher23@gmail.com>
2020-02-05 15:29:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+let destinationURL = "http://localhost:3000/users";
+let configurationObject={
+    method:"POST",
+    headers:{
+      "Content-Type":"application/json",
+      "Accept":"application/json",
+      body:`{
+        name:"Steve",
+        email:"steve@steve.com"
+      }`
+    }
+  }
+  
+  
+function submitData(name,email){
+  fetch(destinationURL,configurationObject).then(function(response){
+    return response.json();
+  }).then(function(obj){
+    console.log(obj);
+  }).catch(function(error){
+    console.log(error.message)
+  })
+}
