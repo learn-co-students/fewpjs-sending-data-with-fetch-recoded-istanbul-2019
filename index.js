d4535e9812fa2ef9cbd8fbe8d72a80c617f43c05
Ammar <ammar.n.sammour@gmail.com>
2020-04-09 16:59:10 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your code here
+const url = "http://localhost:3000/users";
+const submitData = (userName, userEmail) => {
+    let formData = {
+        name: userName,
+        email: userEmail
+    };
+    let config = {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(formData)
+    };
+    return fetch(url, config)
+        .then(res => res.json())
+        .then(json => document.body.append(json.id))
+        .catch(error => document.body.append(error.message))
+}
