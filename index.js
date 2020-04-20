fa01541dea33522d80b7c98ee7fd5c99072eb907
Omar Alithawi <oadnan100@gmail.com>
2020-04-20 20:52:12 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+function submitData(name , email){
+    let Data = {
+        name:name,
+        email:email
+    }
+    
+    let configObj = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(Data)
+      };
+     
+    return fetch("http://localhost:3000/users", configObj)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(object) {
+        let heading = document.createElement('h1');
+        heading.innerHTML = object.id;
+        document.querySelector('body').append(heading);
+      }) 
+      .catch(function(error) {
+        let heading = document.createElement('h1');
+        heading.innerHTML = error.message;
+        document.querySelector('body').append(heading);
+      });
+}
+
+submitData();
