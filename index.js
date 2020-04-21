b0523b0e5feff92990757147c7b3889e557e56e9
mira_arar <mira.ar230@gmail.com>
2020-04-21 15:22:55 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Add your code here
+function submitData(name,email){
+    
+    let userObject={
+        "name": name,
+        "email": email
+    }
+    let configObj = {
+        method: "POST", 
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+            },
+        body: JSON.stringify(userObject)
+        
+    };
+    
+    return fetch("http://localhost:3000/users",configObj).then(function(response){
+        return response.json(); 
+    } )
+    .then(function(response) { 
+            document.write(response.id);
+        // let p = document.createElement('p');
+        // document.querySelector('body').appendChild(p);
+        // p.innerHTML+='<p>"' +response.id+ '"</p>';
+    })
+    .catch(function(error) {
+            document.write(error.message);
+            // let p = document.createElement('p');
+            // document.querySelector('body').appendChild(p);
+            // p.innerHTML+='<p>"' +error.message+ '"</p>';
+            });
+}
+submitData("mira","mira.ar230@gmail.com")
