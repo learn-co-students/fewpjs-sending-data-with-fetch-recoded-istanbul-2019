fd594c6712c03171353b4f2dede5be5574921b68
emrerdem1 <emrerdem94@gmail.com>
2020-04-23 20:12:04 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(name, email) {
+    let postInfo = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    };
+
+    return fetch("http://localhost:3000/users", postInfo)
+        .then(response => response.json())
+        .then(result => {
+            let newP = document.createElement('p');
+            newP.innerHTML = result.id;
+            document.getElementsByTagName("body").appendChild(newP);
+        })
+        .catch(error => {
+            let newE = document.createElement('p');
+            newE.innerHTML = error.message;
+            document.getElementsByTagName("body").appendChild(newE);
+        })
+}
