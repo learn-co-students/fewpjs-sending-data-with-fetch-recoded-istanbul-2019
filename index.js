b19eefe7836613671cae3891b026b880439eb54e
tugba <ttugbasen@gmail.com>
2020-06-24 01:22:27 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Add your code here
+
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+
+
+    .then(response => response.json())
+    .then(obj => {
+        let object = document.createElement("h1");
+        object.innerHTML = obj.id;
+        document.body.appendChild(object);
+    })
+
+    .catch(error => {
+        let object = document.createElement("h1");
+        object.innerHTML = error.message;
+        document.body.appendChild(object);
+        alert("Bad things! Ragnarők!");
+        console.log(error.message);
+
+
+
+
+    })
+
+
+
+}
